5341ca72429c0de79fd0f1761c6a581e50683c4b
Dismeth <sgstubseid@gmail.com>
2018-01-23 10:23:26 +0100

    Removal of redundant code
    Remved redundant code.

--- functions.py
+++ functions.py
@@ -60,26 +60,3 @@ def create_new_image(images):
         result.paste(img, (x, y, x + w, y + h))
 
     result.save(os.path.expanduser('C:\\0Utvikling\\image.jpg'))
-
-
-"""
-files = [
-  '~/Downloads/1.jpg',
-  '~/Downloads/2.jpg',
-  '~/Downloads/3.jpg',
-  '~/Downloads/4.jpg']
-
-result = Image.new("RGB", (800, 800))
-
-for index, file in enumerate(files):
-  path = os.path.expanduser(file)
-  img = Image.open(path)
-  img.thumbnail((400, 400), Image.ANTIALIAS)
-  x = index // 2 * 400
-  y = index % 2 * 400
-  w, h = img.size
-  print('pos {0},{1} size {2},{3}'.format(x, y, w, h))
-  result.paste(img, (x, y, x + w, y + h))
-
-result.save(os.path.expanduser('~/image.jpg'))
-"""
